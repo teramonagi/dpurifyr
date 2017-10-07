6c96bfe5a5c388e4e3665b88fbf220f31fe5b713
Nagi <teramonagi@gmail.com>
2017-10-07 18:00:31 +0900

    Add first concept

---
+++ R/dpurifyr.R
@@ -0,0 +1,10 @@
+#' dpurifyr: a practical way for data preprocessing
+#'
+#' dpurifyr package gives you a practical way for `data preprocessing`
+#' providing a consistent set of verbs that
+#' help you solve the most common data preprocessing challenges.
+#'
+#'
+#' @import rlang
+#' @importFrom stats setNames update
+"_PACKAGE"
